b6bdc8a4841d5388149fb09315dc2e060832d5e5
cjppangan <qcjpangan1@tip.edu.ph>
2023-02-22 11:55:51 +0000

    Activity 4.2

---
+++ test_multiply.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+
+def multiply(x,y):
+
+    return x*y
+
+class MyTest(unittest.TestCase):
+
+    def test(self):
+
+        self.assertEqual(multiply(3,2),7)
+        
+
+if __name__ == '__main__':
+
+    unittest.main()
